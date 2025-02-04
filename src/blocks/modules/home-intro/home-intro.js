16d8e7e9af71d5d70fd34168483fc0f31eb39a5e
FrontMD <web@mockup.digital>
2025-02-04 13:05:55 +0300

    home intro anim in progress

--- src/blocks/modules/home-intro/home-intro.js
+++ src/blocks/modules/home-intro/home-intro.js
@@ -3,11 +3,21 @@ function homeIntro() {
 
     if(!homeIntro) return
 
+    lockBody()
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+
     const homeIntroSlider = homeIntro.querySelector('[data-js="homeIntroSlider"]');
     const homeIntroTabs = homeIntro.querySelector('[data-js="homeIntroTabs"]');
     const homeIntroInfoSlider = homeIntro.querySelector('[data-js="homeIntroInfoSlider"]');
-    const tabsList = homeIntroTabs.querySelectorAll('[data-js="homeIntroTab"]');
+    const progressBar = homeIntro.querySelector('[data-js="homeIntroProgressBar"]')
+    const progressBarFill = homeIntro.querySelector('[data-js="homeIntroProgressBarFill"]')
+    const homeIntroInfo = homeIntro.querySelector('[data-js="homeIntroInfo"]')
 
+    //const tabsList = homeIntroTabs.querySelectorAll('[data-js="homeIntroTab"]');
 
     const homeIntroTabsEx = new Swiper(homeIntroTabs, {
         slidesPerView: 5,
@@ -18,6 +28,9 @@ function homeIntro() {
         slidesPerView: 1,
         allowTouchMove: false,
         effect: 'fade',
+        mousewheel: {
+            releaseOnEdges: true
+        },
         thumbs: {
             swiper: homeIntroTabsEx
         },
@@ -27,10 +40,14 @@ function homeIntro() {
         slidesPerView: 1,
         allowTouchMove: false,
         effect: 'fade',
+        mousewheel: {
+            releaseOnEdges: true
+        }
     })
 
 
     homeIntroInfoSliderEx.controller.control = [homeIntroSliderEx]
+    homeIntroSliderEx.controller.control = [homeIntroInfoSliderEx]
 
     // ищем все анимации
     const animContainers = homeIntroSlider.querySelectorAll('[data-js="homeIntroAnim"]');
@@ -45,40 +62,85 @@ function homeIntro() {
             autoplay: false,
             path: 'public/renders/render-' + index + '/render.json'
         });
-        animItem.setSpeed(0.8)
+        animItem.setSpeed(1)
 
         renders.push(animItem)
     })
 
     renders.forEach((item, index) => {
 
-        
         if(index == 0) {
+            // пока запускаем первую вручную, потом будет после прелоадера
             item.play()
-        }
 
+            // тут надо скрыть вкладки
+            
+            item.addEventListener('config_ready', () => {
+                const totalFrames = item.totalFrames * 0.8;
+
+                item.addEventListener('enterFrame', () => {
+                    let currentProgress = Math.ceil(parseInt(item.currentFrame) / totalFrames * 100);
+
+                    currentProgress = currentProgress > 100 ? 100 : currentProgress
+
+                    progressBarFill.style.width = currentProgress + '%'
+                    
+                    if(currentProgress == 100) {
+                        progressBarToggle(false)
+                    }
+                })
+            })
+    
+
+        }
 
         // после окончания листаем слайдер и перематываем анимацию на определённый кадр, чтобы выглядело как изображение. все кроме 0
         item.addEventListener ('complete', () => {
-            homeIntroInfoSliderEx.slideNext(10, true);
+        
+            // Автопролистывание слайдов
+            //homeIntroInfoSliderEx.slideNext(100, true);
+
             if(index !== 0) {
-                item.goToAndStop(10, true);
+                item.goToAndStop(0, false);
             }
+
         })
+
         
     })
 
+    // смена слайда
     homeIntroSliderEx.on('slideChangeTransitionEnd', function(currentSlider) {
-        renders.forEach(render => {
-            render.goToAndStop(10, true);
 
+        //возвращаем все анимации на старт
+        renders.forEach(render => {
+            render.goToAndStop(0, false);
+        })
+
+        lockBody()
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
         })
 
         const currentSlideIndex = currentSlider.realIndex
 
         renders[currentSlideIndex].play()
+
+        if(currentSlideIndex == renders.length - 1) {
+            setTimeout(unlockBody, 10)
+        }
     });
 
-    console.log(unlockBody)
+    function progressBarToggle(show) {
+        if(show) {
+            progressBar.classList.add('active');
+            homeIntroInfo.classList.remove('active');
+        } else {
+            progressBar.classList.remove('active');
+            homeIntroInfo.classList.add('active');
+        }
+    }
 
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 @@include("./libs/jquery-3.7.0.min.js")
 @@include("./libs/lazyload.min.js")
 @@include("./libs/swiper-bundle.min.js")
+//include("./libs/lottie_api.min.js")
 @@include("./libs/lottie.min.js")
 @@include("./libs/select2.full.min.js")
 @@include("./libs/hystmodal.min.js")
