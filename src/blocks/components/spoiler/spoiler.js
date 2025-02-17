967c6e481e8a0086dc2b8b754606f95598a515d3
FrontMD <web@mockup.digital>
2025-02-17 13:07:43 +0300

    factory in progress

--- src/blocks/components/spoiler/spoiler.js
+++ src/blocks/components/spoiler/spoiler.js
@@ -70,7 +70,7 @@ function spoilers() {
 				}
 			});
 
-
+			spoiler.dispatchEvent(new CustomEvent('spoilerExpand'))
 		};
 	
 		function closeSpoiler(spoiler) {
--- src/blocks/modules/f-factory/f-factory.js
+++ src/blocks/modules/f-factory/f-factory.js
@@ -1,3 +1,36 @@
 function fFactoryController() {
-    
+    // переключение споилеров
+    const slides = document.querySelectorAll('[data-js="tabsBlockSlide"]');
+
+    if(slides) {
+        slides.forEach(slide => {
+            const spoilers = slide.querySelectorAll('[data-js="spoiler"]');
+            const spoilerSides = slide.querySelectorAll('[data-js="spoilerSide"]');
+
+            if(spoilers && spoilerSides) {
+
+                spoilers.forEach(spoiler => {
+                    spoiler.addEventListener('spoilerExpand', (e) => {
+                        const targetId = e.target.dataset.side;
+
+                        if(targetId) {
+                            spoilerSideToggle(spoilerSides, targetId)
+                        }
+                    })
+                })
+
+            }
+        })
+
+        function spoilerSideToggle(list, id) {
+            list.forEach(item => {
+                if(item.dataset.id == id) {
+                    item.classList.add('active')
+                } else {
+                    item.classList.remove('active')
+                }
+            })
+        }
+    }
+
 }
