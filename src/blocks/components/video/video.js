016f4dacb77b5d0bfd215d76d19549baec3c276c
FrontMD <web@mockup.digital>
2025-04-03 16:27:49 +0300

    renders to webp

--- src/blocks/components/video/video.js
+++ src/blocks/components/video/video.js
@@ -8,36 +8,39 @@ function videos() {
         const videoFrame = video.querySelector("[data-js='videoFrame']");
         const videoPoster = video.querySelector("[data-js='videoPoster']");
         const videoPosterSrc = videoPoster.querySelector('img').getAttribute('src')
-    
-        videoPlayBtn.addEventListener('click', () => {
-            videoPlayBtn.remove()
-            videoPoster.remove()
 
-            if(videoFrame.dataset.video.includes('rutube') || videoFrame.dataset.video.includes('vkvideo') || videoFrame.dataset.video.includes('vk.com')) {
-                videoFrame.innerHTML = videoFrame.dataset.video
+        if(videoPlayBtn) {
+            videoPlayBtn.addEventListener('click', () => {
+                videoPlayBtn.remove()
+                videoPoster.remove()
     
-                let player = videoFrame.querySelector('iframe')
-                let playerSrc = player.getAttribute('src')
+                if(videoFrame.dataset.video.includes('rutube') || videoFrame.dataset.video.includes('vkvideo') || videoFrame.dataset.video.includes('vk.com')) {
+                    videoFrame.innerHTML = videoFrame.dataset.video
+        
+                    let player = videoFrame.querySelector('iframe')
+                    let playerSrc = player.getAttribute('src')
+        
+                    if(playerSrc.includes('rutube')) {
+                        player.addEventListener("load", function() {
+                            player.contentWindow.postMessage(JSON.stringify({
+                                type: 'player:play',
+                                data: {}
+                            }), '*');
+                        })
+                    }
+                } else {
+                    const videoLayout = `
+                                            <video controls="controls" poster="${videoPosterSrc}">
+                                                <source src="${videoFrame.dataset.video}" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                                            </video>
+                                        `
+                    videoFrame.innerHTML = videoLayout
     
-                if(playerSrc.includes('rutube')) {
-                    player.addEventListener("load", function() {
-                        player.contentWindow.postMessage(JSON.stringify({
-                            type: 'player:play',
-                            data: {}
-                        }), '*');
-                    })
+                    videoFrame.querySelector('video').play()
                 }
-            } else {
-                const videoLayout = `
-                                        <video controls="controls" poster="${videoPosterSrc}">
-                                            <source src="${videoFrame.dataset.video}" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
-                                        </video>
-                                    `
-                videoFrame.innerHTML = videoLayout
-
-                videoFrame.querySelector('video').play()
-            }
-        })
+            })
+        }
+    
     })
  
 }
