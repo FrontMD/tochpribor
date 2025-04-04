7fa59a2a0ce9b436fb5f8bc7b549aa34604c14b3
FrontMD <web@mockup.digital>
2025-04-04 15:33:42 +0300

    preload images script

--- src/js/preloadAnimImages.js
+++ src/js/preloadAnimImages.js
@@ -2,6 +2,5 @@ const imagesCount = 210
 
 for(let i = 0; i < imagesCount; i++) {
 	let img = new Image();
-	img.src = 'public/renders/render-0/images/seq_0_' + i + '.webp'
-	console.log('грузим картинку ' + i)
+	img.src = '/local/templates/touchpribor/dist/public/renders/render-0/images/seq_0_' + i + '.webp'
 }
