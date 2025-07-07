0bdd5d08359ba3b8757c7b9d29539aef0ff2c4df
FrontMD <web@mockup.digital>
2025-07-07 11:40:53 +0300

    fixes

--- dist/js/preloadAnimImages.min.js
+++ dist/js/preloadAnimImages.min.js
@@ -1,6 +1,15 @@
-const imagesCount = 210
+function preloadAnimImages() {
 
-for(let i = 0; i < imagesCount; i++) {
-	let img = new Image();
-	img.src = '/local/templates/touchpribor/dist/public/renders/render-0/images/seq_0_' + i + '.webp'
+	const homeIntro = document.querySelector('[data-js="homeIntro"]');
+
+    if(!homeIntro) return
+
+	const imagesCount = 210
+	
+	for(let i = 0; i < imagesCount; i++) {
+		let img = new Image();
+		img.src = '/local/templates/touchpribor/dist/public/renders/render-0/images/seq_0_' + i + '.webp'
+	}
 }
+
+preloadAnimImages()
