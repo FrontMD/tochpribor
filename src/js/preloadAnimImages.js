67f2a965d2eef0e56eb65ab37d686e5a8f1c81da
FrontMD <web@mockup.digital>
2025-07-17 14:53:18 +0300

    anim preload fix

--- src/js/preloadAnimImages.js
+++ src/js/preloadAnimImages.js
@@ -1,15 +1,6 @@
-function preloadAnimImages() {
+const imagesCount = 105
 
-	const homeIntro = document.querySelector('[data-js="homeIntro"]');
-
-    if(!homeIntro) return
-
-	const imagesCount = 210
-	
-	for(let i = 0; i < imagesCount; i++) {
-		let img = new Image();
-		img.src = '/local/templates/touchpribor/dist/public/renders/render-0/images/seq_0_' + i + '.webp'
-	}
+for(let i = 0; i < imagesCount; i++) {
+	let img = new Image();
+	img.src = '/local/templates/touchpribor/dist/public/renders/render-0/images/seq_0_' + i + '.webp'
 }
-
-preloadAnimImages()
