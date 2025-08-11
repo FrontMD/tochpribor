5cfdfc50cbae8020232e73227109164937dff7db
FrontMD <web@mockup.digital>
2025-08-11 12:38:30 +0300

    about new intro

--- src/js/import/main.js
+++ src/js/import/main.js
@@ -571,10 +571,6 @@ function textTableScrollInit() {
                             behavior: "smooth"
                         })
                     }
-                    
-
-
-
 
                     let currentPos =  wrapper.scrollLeft;
                     
